0ee54480afe06970117d1f7ba506ea48ebe2db1f
Meghan <meghan.e.mccue@gmail.com>
2020-02-24 14:21:45 +0000

    Extract #make_deposit and #make_withdrawl methods into new UserAccount class

--- lib/user_account.rb
+++ lib/user_account.rb
@@ -1,17 +1,17 @@
-class Bank
+class UserAccount
 
   attr_reader :balance
 
   def initialize
     @balance = 0
-    @account = UserAccount.new
   end
 
   def make_deposit(amount)
-    @balance = @account.make_deposit(amount)
+    @balance += amount
   end
 
   def make_withdrawl(amount)
-    @balance = @account.make_withdrawl(amount)
+    @balance -= amount
   end
+
 end
---
+++ spec/user_account_spec.rb
@@ -0,0 +1,19 @@
+require 'user_account'
+
+describe UserAccount do
+  describe '#make_deposit' do
+    it 'increases balance by given amount' do
+      subject.make_deposit(5)
+      expect(subject.balance).to eq 5
+    end
+  end
+
+  describe '#make_withdrawl' do
+    it 'decreases balance by a given amount' do
+      subject.make_deposit(10)
+      subject.make_withdrawl(5)
+      expect(subject.balance).to eq 5
+    end
+  end
+
+end
