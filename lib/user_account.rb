f46c702ebb4513d3985958f81b2915d1884dbc17
Meghan <meghan.e.mccue@gmail.com>
2020-02-24 16:10:18 +0000

    Refactor updating balance functionality out of #make_deposit and #make_withdrawl into private functions

--- lib/user_account.rb
+++ lib/user_account.rb
@@ -8,16 +8,16 @@ class UserAccount
   end
 
   def make_deposit(amount, transaction = new_transaction)
-    @balance += amount
     transaction.deposit(amount)
     @transactions << transaction
+    increase_balance(amount)
     "Deposit of $#{amount} was successful"
   end
 
   def make_withdrawl(amount, transaction = new_transaction)
-    @balance -= amount
     transaction.withdrawl(amount)
     @transactions << transaction
+    decrease_balance(amount)
     "Withdrawl of $#{amount} was successful"
   end
 
@@ -29,6 +29,14 @@ class UserAccount
 
   def new_transaction
     Transaction.new
+  end
+
+  def increase_balance(amount)
+    @balance += amount
+  end
+
+  def decrease_balance(amount)
+    @balance -= amount
   end
 
 
