8880693f164530d05652ee47703ef15732472dbf
Meghan <meghan.e.mccue@gmail.com>
2020-02-25 12:44:07 +0000

    Bank can print a bank statement

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -19,4 +19,13 @@ describe 'Bank features' do
     expect { bank.print_statement }.to output(statement).to_stdout
   end
 
+  it 'can print a bank statement' do
+    bank = Bank.new
+    @date = Time.now.strftime("%d/%m/%Y")
+    bank.make_deposit(1000)
+    bank.make_withdrawl(500)
+    statement = "date || credit || debit || balance\n#{@date} || || 500.00 || 500.00\n#{@date} || 1000.00 || || 1000.00\n"    
+    expect { bank.print_statement }.to output(statement).to_stdout
+  end
+
 end
