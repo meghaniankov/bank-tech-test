f9aeb6d30f7b8b3071a229e67015fafd23d1b1c9
Meghan <meghan.e.mccue@gmail.com>
2020-02-25 12:43:01 +0000

    Bank can make a withdrawl

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,9 +4,18 @@ describe 'Bank features' do
 
   it 'can make deposit' do
     bank = Bank.new
-    bank.make_deposit(1000)
     @date = Time.now.strftime("%d/%m/%Y")
+    bank.make_deposit(1000)
     statement = "date || credit || debit || balance\n#{@date} || 1000.00 || || 1000.00\n"
+    expect { bank.print_statement }.to output(statement).to_stdout
+  end
+
+  it 'can make a withdrawl' do
+    bank = Bank.new
+    @date = Time.now.strftime("%d/%m/%Y")
+    bank.make_deposit(1000)
+    bank.make_withdrawl(500)
+    statement = "date || credit || debit || balance\n#{@date} || || 500.00 || 500.00\n#{@date} || 1000.00 || || 1000.00\n"    
     expect { bank.print_statement }.to output(statement).to_stdout
   end
 
